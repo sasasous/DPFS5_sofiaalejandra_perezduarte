772ff4374bf5ffdc308660617cd4e9a23b54ecb8
SOuS <t4perezduartesofia@gmail.com>
2025-03-21 14:07:28 -0300

    sprint 5

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const express = require('express')
 const app = express()
+const port = 3000
+const path =
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.ejs')
+})
+
+app.get('/addProduct', (req, res) => {
+  res.sendFile(__dirname + '/views/products/create.ejs')
+})
+
+app.get('/login', (req, res) => {
+  res.send(__dirname + '/views/users/login.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
